c54b84db719fb487b159917714dbfc7e17bccb7f
Rohan929765 <rohan060106@gmail.com>
2026-03-12 22:24:55 +0530

    Add Payment entity class with fields and methods

---
+++ payment.java
@@ -0,0 +1,68 @@
+package com.klef.fsad.exam.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Payment {
+
+    @Id
+    private int paymentId;   // Manual ID (not auto generated)
+
+    private String name;
+    private Date date;
+    private String status;
+    private double amount;
+    private String method;
+
+    public Payment() {}
+
+    public int getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(int paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
