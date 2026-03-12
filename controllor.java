d4c099bfce669ab2827e750fef78cbd129b74a69
Rohan929765 <rohan060106@gmail.com>
2026-03-12 22:26:32 +0530

    Implement PaymentController for payment operations

---
+++ controllor.java
@@ -0,0 +1,29 @@
+package com.klef.fsad.exam.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.klef.fsad.exam.model.Payment;
+import com.klef.fsad.exam.service.PaymentService;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    // POST - Add Payment
+    @PostMapping("/add")
+    public Payment addPayment(@RequestBody Payment payment) {
+        return paymentService.addPayment(payment);
+    }
+
+    // GET - View Payments
+    @GetMapping("/viewall")
+    public List<Payment> getAllPayments() {
+        return paymentService.getAllPayments();
+    }
+}
